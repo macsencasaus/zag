3381a37bb0ae87fc20c7b4139fa4739e22ae6972
Macsen Casaus <macsencasaus@gmail.com>
2025-07-07 10:47:54 -0500

    more pedantic compile flags

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-clang -Iinclude -Icodegen -Ias -ggdb vector.c -o vector
+clang \
+    -Iinclude -Icodegen -Ias \
+    -Werror -Wpedantic -Wall -Wextra \
+    -Wno-bitwise-instead-of-logical -Wno-gnu-folding-constant \
+    -Wno-error=unused-function \
+    -std=c11 \
+    -ggdb \
+    vector.c \
+    -o vector
